1d7cb97483e9a6267522f1f8caf5dd180b00eda0
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-03-10 22:43:04 +0800

    feat:GlobalStyle, style and fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,32 @@
 import Nav from "./components/Nav";
 import Sidebar from "./components/Sidebar";
 import Today from "./components/Today";
+import styled from "styled-components";
+import { GlobalStyle } from "./components/GlobalStyle";
 
 const App = () => {
   return (
     <>
-      <Sidebar />
-      <Nav />
-      <Today />
+      <GlobalStyle />
+      <Wrapper>
+        <Sidebar />
+        <Main>
+          <Nav />
+          <Today />
+        </Main>
+      </Wrapper>
     </>
   );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  background-color: lightcoral;
+`;
+
+const Main = styled.div`
+  background-color: #fff;
+`;
---
+++ client/src/components/GlobalStyle.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary: #70BED1;
+    --completed: rgba(112, 190, 209, 0.35)
+    --primaryShaded: #1C4B56 
+  }
+  * {
+      font-family: 'Roboto', "Open Sans", sans-serif;
+      -webkit-font-smoothing: antialiased;
+  }
+  
+`;
