55fc6d8a1bbc91541cd3eb6fc9c224148c8e9fc8
janson_cheung <jansoncheung0004@gmail.com>
2021-03-10 23:36:30 +0800

    feat: add Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import styled from "styled-components";
+import { FaBell } from "react-icons/fa";
+import Profilephoto from "../assets/ProfilePhoto.png";
 
 const Nav = () => {
   return (
-    <div>
-      <h2>Nav</h2>
-    </div>
+    <CustomContainer>
+      <NavWrapper>
+        {/* <img src={Profilephoto} alt="Profile" /> */}
+        <Profile src={Profilephoto} />
+        <CustomFaBell />
+      </NavWrapper>
+    </CustomContainer>
   );
 };
 
 export default Nav;
+
+const CustomContainer = styled(Container)``;
+const NavWrapper = styled.div`
+  display: flex;
+  color: var(--primaryShaded);
+  justify-content: flex-end;
+  padding-right: 103px;
+  padding-top: 22px;
+`;
+const CustomFaBell = styled(FaBell)`
+  width: 28px;
+  height: 37px;
+`;
+
+const Profile = styled.img`
+  padding-right: 36px;
+`;
