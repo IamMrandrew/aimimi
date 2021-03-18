21d8f53a395eebedb4b252db2b64e41f16af86ee
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-03-19 01:44:26 +0800

    refactor: changed some route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const path = require("path");
 
 require("dotenv").config();
@@ -25,8 +26,10 @@ const app = express();
 //   res.send("Hello World!");
 // });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
 app.use("/user", userRoutes);
 app.use("/goal", goalRoutes);
 
