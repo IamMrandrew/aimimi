59b857cc7c6cb14b0c47f417239c1fe5ac96cc57
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-03-19 01:17:48 +0800

    feat: new route for users

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const UserController = require("../controllers/user");
 const checkAuth = require("../middleware/auth");
 
-router.post("/", UserController.user_signup);
+router.post("/signup", UserController.user_signup);
 
-router.put("/", UserController.user_login);
+router.post("/login", UserController.user_login);
 
 router.delete("/", checkAuth, UserController.user_delete);
 
