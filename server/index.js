7daec27838bd34ac02e8c5c135b4e64f6106370e
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-03-19 21:14:02 +0800

    test: hard code origin

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
+app.use(cors({ credentials: true, origin: "https://aimimi.heroku.com" }));
 app.use(cookieParser());
 
 app.use("/user", userRoutes);
