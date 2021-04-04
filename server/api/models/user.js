7422eca8d91fe484b30733c11660b36da2232118
Tomas2050 <adcapc@gmail.com>
2021-04-04 15:46:15 +0800

    feat: user schema, related controllers
    changed user schema(onGoingGoals is a Map now with goal's objectid as key and progress as value)
    changed related controllers to compromise this change(get all goals, get today view)
    optimized today view's calculation of date(eliminate effect of time, now only compare date)

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const GoalsSchema = require('./goal');
+const GoalsSchema = require("./goal");
 
 const userSchema = Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    username: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    joinDate: {type: Date, default: Date.now},
-    completedGoals: [{type: Schema.Types.ObjectId, ref: 'Goal'}],
-    onGoingGoals:  [{type: Schema.Types.ObjectId, ref: 'Goal'}]
+  _id: mongoose.Schema.Types.ObjectId,
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  joinDate: { type: Date, default: Date.now },
+  completedGoals: [{ type: Schema.Types.ObjectId, ref: "Goal" }],
+  onGoingGoals: {
+    type: Map,
+    of: Number,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
